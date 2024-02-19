6a115108a49cad9f8721663be2638dc221763adb
dhyces <10985914+dhyces@users.noreply.github.com>
2024-02-19 17:02:26 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Forge Template by Dhyces
---
+++ src/main/resources/mods.groovy
@@ -0,0 +1,26 @@
+ModsDotGroovy.make {
+    modLoader = "javafml"
+    loaderVersion = "[${(this.libs.versions.forge as String).split("\\.")[0]},)"
+
+    license = this.buildProperties["mod_license"]
+    issueTrackerUrl = this.buildProperties["mod_issues_url"]
+
+    mod {
+        modId = this.buildProperties["mod_id"]
+        displayName = this.buildProperties["mod_name"]
+        
+        displayUrl =  this.buildProperties["mod_display_url"]
+
+        version = this.version
+
+        description =
+        authors = (this.buildProperties["authors"] as String).split(",")
+
+        logoFile = "assets/${modId}/logo.png"
+
+        dependencies {
+            forge = "[${this.forgeVersion},)"
+            minecraft = this.minecraftVersionRange
+        }
+    }
+}
